273541e24bc4b09509508239b80efc084677469b
ylin1210 <114723092+ylin1210@users.noreply.github.com>
2022-11-03 16:08:13 +0800

    V1

---
+++ 正式版首頁/frontpage.js
@@ -0,0 +1,18 @@
+
+var curindex=0;
+var imgtxt=["A","B","C","D"];
+var imgpath=["lunbo1.png","lunbo2.png","lunbo3.png","lunbo4.png"];
+
+function b(x){
+	curindex=x;
+	document.getElementById("a").src=imgpath[curindex];
+	document.getElementById("txt").innerHTML=imgtxt[curindex];
+}
+function l(){
+	curindex=(curindex+3)%4;
+	b(curindex);
+}
+function r(){
+	curindex=(curindex+1)%4;
+	b(curindex);
+}
